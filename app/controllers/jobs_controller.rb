7c10cc4f6812793948336cf2f90033a3ab2a1d82
Clive <starlwj@gmail.com>
2017-01-08 08:45:06 +0800

    add jobs#update& jobs#destroy functions

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -22,6 +22,27 @@ class JobsController < ApplicationController
 
   end
 
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+
+    if @job.update(job_params)
+      redirect_to jobs_path
+    else
+      render :edit
+    end
+
+    def destroy
+      @job = Job.find(params[:id])
+      @job.destroy
+      
+      redirect_to jobs_path
+    end
+  end
+
   private
 
   def job_params
