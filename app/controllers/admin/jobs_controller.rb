6c6077b215063a310875e8141387008b71b89061
Clive <starlwj@gmail.com>
2017-01-10 06:44:49 +0800

    redo admin login authenticate in better way

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -49,7 +49,7 @@ class Admin::JobsController < ApplicationController
   end
 
   def require_is_admin
-    if current_user.email != 'starlwj@sina.com'
+    if !current_user.admin?
       flash[:alert] = "You are not admin"
       redirect_to root_path
     end
